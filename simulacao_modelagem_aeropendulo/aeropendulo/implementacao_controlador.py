9fd6400f216ddbca2cd9ae6314dbd2af7d3462e6
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-05 00:19:57 -0300

    repositorio atualizado

--- simulacao_modelagem_aeropendulo/aeropendulo/implementacao_controlador.py
+++ simulacao_modelagem_aeropendulo/aeropendulo/implementacao_controlador.py
@@ -15,6 +15,9 @@
 #
 
 class ControladorDiscreto:
+    """
+        Classe que implementa os controladores para o sistema.
+    """
     def __init__(self, referencia=1, T=0.0625):
         self.uk = 0
         self.uk1 = 0
@@ -31,18 +34,32 @@ class ControladorDiscreto:
 
     # disponibiliza o sinal de controle
     def get_u(self):
+        """
+            Método que retorna o sinal de controle.
+        Returns:
+            float.
+        """
         return self.uk
 
-    # Calcula o sinal de controle Proporcional Integral
+    # Calcula o sinal de controle Proporcional Integral.
     def control_pi(self):
+        """
+        Método que implementa o controle Proporcional Integral discretizado.
+        Returns:
+            None.
+        """
         self.ek = self.r - self.yout
         self.uk = self.uk1 + 0.2165 * self.ek - 0.2087 * self.ek1
         self.ek1 = self.ek
         self.uk1 = self.uk
         self.k = self.k + 1
 
-        # Calcula o sinal de controle Proporcional
     def controle_proporcional(self, kp=1.0):
+        """
+        Método que implementa o controle Proporcional.
+        Returns:
+            None.
+        """
         self.ek = self.r - self.yout
         self.KP = kp
         self.uk = self.KP * self.ek
