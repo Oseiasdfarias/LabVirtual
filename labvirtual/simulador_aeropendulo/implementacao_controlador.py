ef7713f806d993d07965f1f775d6a2f301075523
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-05-11 09:59:28 -0300

    repositorio atualizado

--- labvirtual/simulador_aeropendulo/implementacao_controlador.py
+++ labvirtual/simulador_aeropendulo/implementacao_controlador.py
@@ -17,8 +17,13 @@
 class ControladorDiscreto:
     """
         Classe que implementa os controladores para o sistema.
+        Args:
+            referencia: int - Sinal de referência para o controlador seguir.
+            T: Período de amostragem do controlador.
+        Return:
+            None
     """
-    def __init__(self, referencia=1, T=0.0625):
+    def __init__(self, referencia: int = 1, T: float = 0.0625) -> None:
         self.uk = 0
         self.uk1 = 0
         self.ek = 0
@@ -29,24 +34,32 @@ class ControladorDiscreto:
         self.T = T
 
     # Pega o sinal do sensor
-    def set_sensor(self, yout):
+    def set_sensor(self, yout: float = 1) -> None:
+        """
+            Recebe o sinal de saída, o ângulo do braço do aeropêndulo.
+        Args:
+            yout: Sinal do sensor de ângulo.
+        Return:
+            None
+        """
         self.yout = yout
 
     # disponibiliza o sinal de controle
-    def get_u(self):
+    def get_u(self) -> float:
         """
             Método que retorna o sinal de controle.
-        Returns:
-            float.
+        Return:
+            float: Sinal de controle.
         """
         return self.uk
 
     # Calcula o sinal de controle Proporcional Integral.
-    def control_pi(self):
+    def control_pi(self) -> None:
         """
-        Método que implementa o controle Proporcional Integral discretizado.
-        Returns:
-            None.
+            Método que implementa o controle Proporcional Integral
+            discretizado.
+        Return:
+            None
         """
         self.ek = self.r - self.yout
         self.uk = self.uk1 + 0.2165 * self.ek - 0.2087 * self.ek1
@@ -56,9 +69,11 @@ class ControladorDiscreto:
 
     def controle_proporcional(self, kp=1.0):
         """
-        Método que implementa o controle Proporcional.
-        Returns:
-            None.
+            Método que implementa o controle Proporcional discretizado.
+        Args:
+            kp: Ganho do controlador Proporcional.
+        Return:
+            None
         """
         self.ek = self.r - self.yout
         self.KP = kp
